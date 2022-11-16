52f3dee8c503c2898ee5a55eeb30cb88f742b66c
Giacomo Surace <surax@MacBook-Air-di-Giacomo.local>
2022-11-16 01:08:40 +0100

    something

--- main.go
+++ main.go
@@ -5,93 +5,202 @@ import (
 	"io/fs"
 	"log"
 	"net/http"
+
+	"context"
+
+	kserveapi "github.com/kserve/kserve/pkg/apis/serving/v1beta1"
+	servingv1beta1 "github.com/kserve/kserve/pkg/client/clientset/versioned/typed/serving/v1beta1"
+	kserveconstants "github.com/kserve/kserve/pkg/constants"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 //go:embed static/*
 var static embed.FS
 
-// TODO: implement list_isvc
-// var r = k8sCustomClient.listNamespacedCustomObject('serving.kserve.io','v1beta1','default', "inferenceservices")
-// .then(result => res.status(200).json(result.body))
-// .catch(err => res.status(500).send(err));
+func list_isvc(client *servingv1beta1.ServingV1beta1Client, ctx context.Context, namespace string) ([]kserveapi.InferenceService, error) {
+	isvc_list, err := client.InferenceServices(namespace).List(ctx, metav1.ListOptions{})
+	isvc_list_new := make([]kserveapi.InferenceService, len(isvc_list.Items))
 
-// TODO: implement delete_isvc
-// k8sCustomClient.deleteNamespacedCustomObject('serving.kserve.io','v1beta1','default', "inferenceservices",req.body.isvcname)
-// .then((result)=>{
-// 	//console.log(result)
-// 	res.status(200).json({"message": "Deleting inference service " + result.body.metadata.name})
-// })
-// .catch((err)=>{
-// 	//console.log(err.body.message)
-// 	res.status(err.statusCode).json({ 'message': 'ERROR: ' + err.body.message })
-// })
+	for i := 0; i < len(isvc_list.Items); i++ {
+		switch isvc_list.Items[i].Spec.Predictor.Model.ModelFormat.Name {
 
-// TODO: implement create_isvc
-// switch (req.body.isvctype) {
-// case "onnx":
-//   var body = {
-// 	"apiVersion": "serving.kserve.io/v1beta1",
-// 	"kind": "InferenceService",
-// 	"metadata": {
-// 		"name": req.body.isvcname,
-// 	},
-// 	"spec": {
-// 		"predictor": {
-// 		  [req.body.isvctype]: {
-// 			"protocolVersion": "v2",
-// 			"storageUri": req.body.url,
-// 			"args": ["--strict-model-config=false"]
-// 		  }
-// 		}
-// 	}
-//   }
-//   break;
-// case "xgboost":
-// case "sklearn":
-//   var body = {
-// 	"apiVersion": "serving.kserve.io/v1beta1",
-// 	"kind": "InferenceService",
-// 	"metadata": {
-// 		"name": req.body.isvcname,
-// 	},
-// 	"spec": {
-// 		"predictor": {
-// 		  [req.body.isvctype]: {
-// 			"protocolVersion": "v2",
-// 			"storageUri": req.body.url,
-// 		  }
-// 		}
-// 	}
-//   }
-//   break;
-// case "tensorflow":
-//   var body = {
-// 	"apiVersion": "serving.kserve.io/v1beta1",
-// 	"kind": "InferenceService",
-// 	"metadata": {
-// 		"name": req.body.isvcname,
-// 	},
-// 	"spec": {
-// 		"predictor": {
-// 		  [req.body.isvctype]: {
-// 			"protocolVersion": "v1",
-// 			"storageUri": req.body.url
-// 		  }
-// 		}
-// 	  }
-//   }
-//   break;
-// }
+		case "onnx":
+			isvc_list_new[i] = kserveapi.InferenceService{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "serving.kserve.io/v1beta1",
+					Kind:       "InferenceService",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      isvc_list.Items[i].Name,
+					Namespace: isvc_list.Items[i].Namespace,
+				},
+				Spec: kserveapi.InferenceServiceSpec{
+					Predictor: kserveapi.PredictorSpec{
+						ONNX: &kserveapi.ONNXRuntimeSpec{
+							PredictorExtensionSpec: kserveapi.PredictorExtensionSpec{
+								ProtocolVersion: isvc_list.Items[i].Spec.Predictor.Model.ProtocolVersion,
+								StorageURI:      isvc_list.Items[i].Spec.Predictor.Model.StorageURI,
+								Container: v1.Container{
+									Args: isvc_list.Items[i].Spec.Predictor.Model.Args,
+								},
+							},
+						},
+					},
+				},
+			}
 
-// k8sCustomClient.createNamespacedCustomObject('serving.kserve.io','v1beta1','default', "inferenceservices",body)
-//   .then((result)=>{
-// 	  //console.log(result)
-// 	  res.status(200).json({"message": "CREATED: inference service " + result.body.metadata.name})
-//   })
-//   .catch((err)=>{
-// 	  //console.log(err.body.message)
-// 	  res.status(err.statusCode).json({ 'message': 'ERROR: ' + err.body.message })
-//   })
+		case "xgboost":
+		case "sklearn":
+			isvc_list_new[i] = kserveapi.InferenceService{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "serving.kserve.io/v1beta1",
+					Kind:       "InferenceService",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      isvc_list.Items[i].Name,
+					Namespace: isvc_list.Items[i].Namespace,
+				},
+				Spec: kserveapi.InferenceServiceSpec{
+					Predictor: kserveapi.PredictorSpec{
+						SKLearn: &kserveapi.SKLearnSpec{
+							PredictorExtensionSpec: kserveapi.PredictorExtensionSpec{
+								ProtocolVersion: isvc_list.Items[i].Spec.Predictor.Model.ProtocolVersion,
+								StorageURI:      isvc_list.Items[i].Spec.Predictor.Model.StorageURI,
+							},
+						},
+					},
+				},
+			}
+		case "tensorflow":
+			isvc_list_new[i] = kserveapi.InferenceService{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "serving.kserve.io/v1beta1",
+					Kind:       "InferenceService",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      isvc_list.Items[i].Name,
+					Namespace: isvc_list.Items[i].Namespace,
+				},
+				Spec: kserveapi.InferenceServiceSpec{
+					Predictor: kserveapi.PredictorSpec{
+						Tensorflow: &kserveapi.TFServingSpec{
+							PredictorExtensionSpec: kserveapi.PredictorExtensionSpec{
+								ProtocolVersion: isvc_list.Items[i].Spec.Predictor.Model.ProtocolVersion,
+								StorageURI:      isvc_list.Items[i].Spec.Predictor.Model.StorageURI,
+							},
+						},
+					},
+				},
+			}
+		}
+	}
+
+	if err != nil {
+		return nil, err
+	}
+	return isvc_list_new, nil
+}
+
+func delete_isvc(client *servingv1beta1.ServingV1beta1Client, ctx context.Context, namespace string, name string) error {
+	err := client.InferenceServices(namespace).Delete(ctx, name, metav1.DeleteOptions{})
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func create_isvc(ctx context.Context, isvcName string, name string, uri string, client *servingv1beta1.ServingV1beta1Client, namespace string) (string, error) {
+	var svc kserveapi.InferenceService
+	switch isvcName {
+	case "onnx":
+		protocol := kserveconstants.ProtocolV2
+
+		svc = kserveapi.InferenceService{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "serving.kserve.io/v1beta1",
+				Kind:       "InferenceService",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      name,
+				Namespace: "default",
+			},
+			Spec: kserveapi.InferenceServiceSpec{
+				Predictor: kserveapi.PredictorSpec{
+					ONNX: &kserveapi.ONNXRuntimeSpec{
+						PredictorExtensionSpec: kserveapi.PredictorExtensionSpec{
+							ProtocolVersion: &protocol,
+							StorageURI:      &uri,
+							Container: v1.Container{
+								Args: []string{
+									"--strict-model-config=false",
+								},
+							},
+						},
+					},
+				},
+			},
+		}
+
+	case "xgboost":
+	case "sklearn":
+
+		protocol := kserveconstants.ProtocolV2
+
+		svc = kserveapi.InferenceService{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "serving.kserve.io/v1beta1",
+				Kind:       "InferenceService",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      name,
+				Namespace: "default",
+			},
+			Spec: kserveapi.InferenceServiceSpec{
+				Predictor: kserveapi.PredictorSpec{
+					SKLearn: &kserveapi.SKLearnSpec{
+						PredictorExtensionSpec: kserveapi.PredictorExtensionSpec{
+							ProtocolVersion: &protocol,
+							StorageURI:      &uri,
+						},
+					},
+				},
+			},
+		}
+
+	case "tensorflow":
+		protocol := kserveconstants.ProtocolV1
+
+		svc = kserveapi.InferenceService{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "serving.kserve.io/v1beta1",
+				Kind:       "InferenceService",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      name,
+				Namespace: "default",
+			},
+			Spec: kserveapi.InferenceServiceSpec{
+				Predictor: kserveapi.PredictorSpec{
+					Tensorflow: &kserveapi.TFServingSpec{
+						PredictorExtensionSpec: kserveapi.PredictorExtensionSpec{
+							ProtocolVersion: &protocol,
+							StorageURI:      &uri,
+						},
+					},
+				},
+			},
+		}
+	}
+
+	_, err := client.InferenceServices(namespace).Create(ctx, &svc, metav1.CreateOptions{})
+	if err != nil {
+		return "", err
+	} else {
+		return "", nil
+	}
+}
 
 func main() {
 	content, _ := fs.Sub(static, "static")
